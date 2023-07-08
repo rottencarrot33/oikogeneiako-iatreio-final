419cc6a02a4e5bbd64b9124092db560924fab2e5
rottencarrot33 <122114827+rottencarrot33@users.noreply.github.com>
2023-07-08 17:30:49 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,9 +1,16 @@
 <?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = "website request";
     $message = $_POST['message'];
+    
+    // Honeypot field
+    $honeypot = $_POST['honeypot'];
+    if (!empty($honeypot)) {
+        // If honeypot field is not empty, it means a bot filled it out, so ignore the submission
+        return;
+    }
 
     $recipient = $_POST['recipient'];
     $to = $recipient;
@@ -11,9 +18,9 @@
     $headers .= "Reply-To: $email\r\n";
 
     if (mail($to, $subject, $message, $headers)) {
-      header("Location: thankyou.html");
+        header("Location: thankyou.html");
     } else {
-      echo "There was a problem sending the message.";
+        echo "There was a problem sending the message.";
     }
-  }
+}
 ?>
